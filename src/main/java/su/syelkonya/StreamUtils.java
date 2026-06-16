afa53df176a7a7bf69b63b7c5baf89f333f4f4cd
Syelkonya <teldar07@gmail.com>
2026-06-16 21:44:32 +0300

    add firstLetterSet

--- src/main/java/su/syelkonya/StreamUtils.java
+++ src/main/java/su/syelkonya/StreamUtils.java
@@ -2,28 +2,37 @@ package su.syelkonya;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StreamUtils {
 
-    public int returnEvenNumbersCount(List<Integer> integerList){
+    public int returnEvenNumbersCount(List<Integer> integerList) {
         return integerList.stream()
                 .filter(n -> n % 2 == 0)
                 .mapToInt(n -> n)
                 .sum();
     }
 
-    public List<String> allToUpperCase(List<String> names){
+    public List<String> allToUpperCase(List<String> names) {
         return names.stream()
                 .map(String::toUpperCase)
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
-//  Реализовать метод, который возвращает кол-во строк в списке длиннее n символов.
-    public int countLinesLongerThan(List<String> lines, int n){
+    //  Реализовать метод, который возвращает кол-во строк в списке длиннее n символов.
+    public int countLinesLongerThan(List<String> lines, int n) {
         return (int) lines.stream()
                 .filter(l -> l.length() > n)
                 .count();
+    }
+
+    //    Реализовать метод, который возвращает set первых букв каждого слова из списка.
+    public Set<Character> firstLetterSet(List<String> words) {
+        return words.stream()
+                .filter(w -> !w.isEmpty())
+                .map(w -> w.charAt(0))
+                .collect(Collectors.toSet());
     }
 
 }
